1cb7b54d55ba88b3f9bfefe35234b67e166ca975
Sam Stouffer <samuellstouffer@gmail.com>
2020-08-18 12:12:29 -0400

    Replace test file copying with test file config

--- .hygiene/copy-to-container.sh
+++ .hygiene/copy-to-container.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 docker cp . hygiene:/input/ontology-hygiene-tests
-docker exec hygiene bash -c 'mv "${INPUT:?}/${ONTPUB_FAMILY:?}"/.hygiene/tests/* "${INPUT:?}/${ONTPUB_FAMILY:?}"/etc/testing/hygiene'
--- .hygiene/create-directories.sh
+++ .hygiene/create-directories.sh
@@ -3,11 +3,6 @@
 # local output dir
 mkdir /output
 
-# temp solution for tests location
-mkdir etc
-mkdir etc/testing
-mkdir etc/testing/hygiene
-
 # container dirs
 docker exec hygiene mkdir /input
 docker exec hygiene mkdir /output
